d2d739b8b252a45a3706e905b754dbf845f255a8
SK3620 <k.n.t11193620@gmail.com>
2024-11-24 19:59:24 +0900

    Add: ロケーション情報に国旗画像URLをキーとして追加

--- fixedLocationsUpdaterDev.js
+++ fixedLocationsUpdaterDev.js
@@ -1,8 +1,11 @@
-// Firebase Admin SDKをインポート
-const admin = require("firebase-admin");
+import { fixedLocations } from "./fixedLocations.js";
+import admin from "firebase-admin";
+import { readFileSync } from "fs";
 
-// Firebaseプロジェクトのサービスアカウントキーのパスを指定
-const serviceAccount = require("/Users/suzukikenta/Desktop/Development/Swift-Projects/Nomad-Learner-Dev-FixedLocationsUpdater/nomad-learnerdev-88484-firebase-adminsdk-w9mou-f2ec32451b.json"); // サービスアカウントキーのパス
+// サービスアカウントキーのパス
+const serviceAccountPath =
+  "/Users/suzukikenta/Desktop/Development/Swift-Projects/Nomad-Learner-Dev-FixedLocationsUpdater/nomad-learnerdev-88484-firebase-adminsdk-w9mou-f2ec32451b.json";
+const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf8"));
 
 // Firebaseを初期化（DatabaseURLを追加）
 admin.initializeApp({
@@ -13,21 +16,11 @@ admin.initializeApp({
 // Realtime Databaseのインスタンスを取得
 const db = admin.database();
 
-const fs = require("fs");
-
-// fixedLocation.jsonファイルを読み込み、locationsオブジェクトを取得
-const dataToUpload = JSON.parse(
-  fs.readFileSync(
-    "/Users/suzukikenta/Desktop/Development/Swift-Projects/Nomad-Learner-Dev-fixedLocationsUpdater/fixedLocations.json",
-    "utf-8"
-  )
-).fixedLocations;
-
 // データをRealtime Databaseに一括アップロードする非同期関数を定義
 async function uploadData() {
   try {
     // `fixedLocations`ノードに一括でデータを設定
-    await db.ref("fixedLocations").set(dataToUpload);
+    await db.ref("fixedLocations").set(fixedLocations);
     console.log("すべてのデータが一括で追加されました。");
   } catch (error) {
     console.error("データの一括追加中にエラーが発生しました:", error);
